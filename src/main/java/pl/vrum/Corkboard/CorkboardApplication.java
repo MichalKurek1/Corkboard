d0ddde396b60915801570d83df3f01042361c3da
MichalKurek1 <michal.kurek1@interia.pl>
2018-06-01 12:39:09 +0200

    First

---
+++ src/main/java/pl/vrum/Corkboard/CorkboardApplication.java
@@ -0,0 +1,12 @@
+package pl.vrum.Corkboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorkboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorkboardApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/vrum/Corkboard/CorkboardApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.vrum.Corkboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CorkboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
